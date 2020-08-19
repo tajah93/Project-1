ca21f358a21edd8c8663dc7cdf88fc5f8f5a180b
Cris Perez <cristino.perez525@gmail.com>
2020-08-19 17:19:36 -0500

    Got the scheduler modal set up to finally record scheduled items in local storage, also list out stored workouts and dynamically update as workouts are added. At the moment, buttons only are created upon adding workouts to the schedule, will work on rendering buttons on load from local storage

--- scheduler.js
+++ scheduler.js
@@ -10,45 +10,92 @@
 // $("#time").text(m);
 
 // });
- 
+
 
 var dayBlock = $(".Days")
 var workoutModal = $("#scheduler-modal")
 var inputArea = $("#textarea")
 var modalSubmit = $("#submit-workout")
 var closeBtn = $(".close")
-var workoutList = $(".workout-text")
+var modalText = $(".modal-text")
+var workoutList = $(".workout-list")
 var numberCheck = RegExp(/[0-9]/)
 
 for (i = 0; i < dayBlock.length; i++) {
-    
+
     var blockToWrite = dayBlock[i]
     var dayTxt = blockToWrite.innerHTML
     var dayDt = moment(dayTxt, "dddd").format("L")
     var dateDisplay = $("<p>").addClass("day-date")
     dateDisplay.text(dayDt)
-    
+
     blockToWrite.append(", " + dateDisplay.text())
 }
 
 
-$(".workoutPlan").on("click", function() { 
+$(".workoutPlan").on("click", function () {
 
-    var scheduleDt = $(this).prev().text() 
+    var scheduleDt = $(this).prev().text()
     console.log(moment(scheduleDt, "dddd, L").format("dddd, L"))
 })
 
-dayBlock.on("click", function() {
+dayBlock.on("click", function () {
 
+    inputArea.empty()
+    var workoutBlock = $(this).next()
     var clickedDt = $(this).text()
-    workoutList.text("Scheduled workouts for " + clickedDt + ": ")
+    console.log(clickedDt)
+
+    modalText.text("Scheduled workouts for " + clickedDt + ": ")
+
+    modalSubmit.unbind().click(function() {
+
+        event.stopImmediatePropagation()
+        event.preventDefault()
+
+        console.log(clickedDt)
+
+        console.log("click1")
+        var addedWorkout = inputArea.val()
+        var workoutBtn = $("<button>")
+        workoutBtn.text(addedWorkout)
+        workoutBlock.append(workoutBtn)
+
+        if (workoutList.text() === "") {
+
+            workoutList.append(addedWorkout)
+        } else {
+            workoutList.append(", " + addedWorkout)
+        }
+
+        localStorage.setItem(clickedDt, workoutList.text())
+    })
+
+    for (i = 0; i < localStorage.length; i++) {
+
+        if (clickedDt == localStorage.key(i)) {
+
+            workoutList.text(localStorage.getItem(clickedDt))
+            console.log(localStorage.getItem(clickedDt))
+            break;
+        } else {
+            workoutList.text("")
+            console.log(localStorage.getItem(clickedDt))
+        }
+    }
+
     workoutModal.show()
 
+    closeBtn.one("click", function () {
+
+        workoutModal.hide()
+        clickedDt - ""
+    })
 })
 
-closeBtn.on("click", function() {
-    workoutModal.hide()
-})
+
+
+
 
 
 
@@ -86,7 +133,7 @@ function embedVideo(data) {
         var currentIframe = $("<iframe>").attr("data-log", i)
 
         $(currentIframe).attr('src', 'https://www.youtube.com/embed/' + data.items[i].id.videoId)
-        $("body").append(videoDiv.append(videoTitle,currentIframe))
+        $("body").append(videoDiv.append(videoTitle, currentIframe))
         console.log(data.items[i])
     }
 }
