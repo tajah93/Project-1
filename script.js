1608ed43c8f7336015c39d373060579438f6441d
Cris Perez <cristino.perez525@gmail.com>
2020-08-18 21:31:42 -0500

    Added logic to hide get started button when appropriate as well as local storage of user name

--- script.js
+++ script.js
@@ -36,4 +36,20 @@ function embedVideo(data) {
     }
 }
 
-getYoutube()
+
+var givenName = $(".name-greeting")
+$(".get-started").on("click", function() {
+
+    var userName = prompt("What is your name?")
+    localStorage.setItem("User Name", userName)
+    $(".get-started").hide()
+
+    var greeting = "Hello, " + userName + "!"
+    givenName.text(greeting).css("font-size", "xx-large")
+})
+
+console.log(localStorage.getItem("User Name"))
+
+if (localStorage.getItem("User Name") !== null) {
+    $(".start-btn").hide()
+}
