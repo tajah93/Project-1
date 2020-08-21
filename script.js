5066fa77331d7daae325a625f78ca5920650efd0
Cris Perez <cristino.perez525@gmail.com>
2020-08-21 15:36:59 -0500

    Created giphy api call upon name submit on get started button, added element classes for easier removal from modal

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function renderGreeting() {
     var greetingName = localStorage.getItem("username")
 
     if (localStorage.getItem("username") !== null) {
-        
+
         getStartedBtn.hide()
         greeting.text("Hello, " + greetingName + "!")
     }
@@ -28,10 +28,29 @@ modalSubmit.on("click", function () {
     var userName = inputArea.val()
     localStorage.setItem("username", userName)
     renderGreeting()
-    homeModal.hide()
+
+    $(".modal-text").remove()
+    $("#textarea").remove()
+    $("br").remove()
+    modalSubmit.remove()
+    
+    var giphyApiUrl = "https://api.giphy.com/v1/gifs/1PMVNNKVIL8Ig?api_key=B7Bohxdra5D2a0c4y21liQiL2KoBe4LS";
+    
+    $.ajax({
+        url: giphyApiUrl,
+        method:"GET"
+    }).then(function(response){
+
+        var welcome = $("<h3>").addClass("modal-welcome")
+        welcome.text("Welcome to trim(fit), " + userName + "!")
+
+        var gifEl = $("<img>").attr("src", response.data.images.fixed_height.url)
+
+        $(".modal-content").append(welcome,gifEl)
+    })
 })
 
-closeBtn.on("click", function(){
+closeBtn.on("click", function () {
     homeModal.hide()
 })
 
