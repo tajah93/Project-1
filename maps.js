f151ea2c612841959c0b785687935a9270b14f37
Tajah Johnson <tajah93@hotmail.com>
2020-08-22 17:01:22 -0500

    Changed RreadMe

---
+++ maps.js
@@ -0,0 +1,10 @@
+var searchZipBtn = $("#btn-zipCode");
+
+searchZipBtn.on("click", function() {
+    event.preventDefault();
+    var zipCode = $("#zipCode").val();
+    console.log(zipCode);
+    var url = "https://www.google.com/maps/embed/v1/search?key=AIzaSyCOuWxKuOIXPqWS8VpnyY_F7zQPh4QWu2Y&q=parks+fitness+centers+near+" + zipCode;
+    $("#gMap").attr("src", url);
+    
+})
