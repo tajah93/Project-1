fa0034559b21e8a89d1f01f3f0a74ebcba9b720b
Cris Perez <cristino.perez525@gmail.com>
2020-08-22 11:53:14 -0500

    added workout reminder to greeting on home page

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ function renderGreeting() {
     if (localStorage.getItem("username") !== null) {
 
         getStartedBtn.hide()
-        greeting.text("Hello, " + greetingName + "!")
+
+        for (s = 0; s < localStorage.length; s++) {
+
+            var currentDayDt = moment().format("dddd, L")
+
+            if (moment(localStorage.key(s)).format("dddd, L") === currentDayDt) {
+
+                greeting.text("Hello, " + greetingName + "! You have workouts scheduled today! Please see the scheduler page.")
+                break;
+            } else (
+                greeting.text("Hello, " + greetingName + "! You don't have any workouts scheduled today!")
+            )
+
+        }
+        
     }
 }
 
@@ -33,20 +47,20 @@ modalSubmit.on("click", function () {
     $("#textarea").remove()
     $("br").remove()
     modalSubmit.remove()
-    
+
     var giphyApiUrl = "https://api.giphy.com/v1/gifs/1PMVNNKVIL8Ig?api_key=B7Bohxdra5D2a0c4y21liQiL2KoBe4LS";
-    
+
     $.ajax({
         url: giphyApiUrl,
-        method:"GET"
-    }).then(function(response){
+        method: "GET"
+    }).then(function (response) {
 
         var welcome = $("<h3>").addClass("modal-welcome")
         welcome.text("Welcome to trim(fit), " + userName + "!")
 
         var gifEl = $("<img>").attr("src", response.data.images.fixed_height.url)
 
-        $(".modal-content").append(welcome,gifEl)
+        $(".modal-content").append(welcome, gifEl)
     })
 })
 
